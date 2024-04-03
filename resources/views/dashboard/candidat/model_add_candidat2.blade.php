8821b46bf696ba2a83513e5244a361ac3b06c0ec
MohammedAbdo1 <mohammed775163@gmail.com>
2024-04-03 05:06:59 +0300

    update

---
+++ resources/views/dashboard/candidat/model_add_candidat2.blade.php
@@ -0,0 +1,29 @@
+<!-- add modal -->
+<div class="modal" id="model_add_candidat2">
+			<div class="modal-dialog " role="document">
+				<div class="modal-content modal-content-demo">
+					<div class="modal-header">
+						<h5 class="modal-title"> اضافة مشارك </h5><button aria-label="Close" class="close" data-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button>
+					</div>
+					<form action="">
+					    <div class="modal-body">
+					    	<div class="row">
+					    		<div class="col-12">
+					    			<label for="example"> الأسم  </label>
+                                    <select name="form-control" id="">
+                                        <option value="">1</option>
+                                        <option value="">2</option>
+                                    </select>
+					    			<input class="form-control"  required="" type="text">
+					    		</div>
+					    	</div>
+					    </div>
+					    <div class="modal-footer border-0">
+					    	<button class="btn btn-warning-gradient btn-with-icon" type="submit"> حفظ <i class="bi bi-floppy"></i></button>
+					    	<button class="btn ripple btn-secondary" data-dismiss="modal" type="button"> إلغاء </button>
+					    </div>
+					</form>
+				</div>
+			</div>
+		</div>
+		<!-- End add modal -->
