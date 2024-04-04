43652b3c6c9056e242e1ac2adde920910267165c
Hassan Elhallaq <hassanelhallaq@Hassans-MacBook-Pro.local>
2024-04-04 10:54:56 +0200

    edits

--- database/migrations/2024_04_04_115252_add_is_accept_to_candidate_courses_table.php
+++ database/migrations/2024_04_04_115252_add_is_accept_to_candidate_courses_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('candidate_courses', function (Blueprint $table) {
-            //
+            $table->integer('is_accept')->default(0);
+
         });
     }
 
